fabd49d34c976dc2b4fa347251f83b52ac33d575
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-30 19:21:23 +0000

    brew vendor-gems: commit updates.

--- Library/Homebrew/vendor/bundle/ruby/3.4.0/gems/public_suffix-6.0.2/lib/public_suffix/version.rb
+++ Library/Homebrew/vendor/bundle/ruby/3.4.0/gems/public_suffix-6.0.2/lib/public_suffix/version.rb
@@ -4,11 +4,11 @@
 #
 # Domain name parser based on the Public Suffix List.
 #
-# Copyright (c) 2009-2024 Simone Carletti <weppos@weppos.net>
+# Copyright (c) 2009-2025 Simone Carletti <weppos@weppos.net>
 
 module PublicSuffix
 
   # @return [String] the current library version
-  VERSION = "6.0.1"
+  VERSION = "6.0.2"
 
 end

fa26976b1db9c058a16cbfa540d4f79925ae7ed6
botantony <antonsm21@gmail.com>
2025-05-03 00:36:48 +0200

    autobump_constants: add `bumped_by_upstream` reason
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Library/Homebrew/autobump_constants.rb
+++ Library/Homebrew/autobump_constants.rb
@@ -4,4 +4,5 @@
 # TODO: add more reasons here
 NO_AUTOBUMP_REASONS_LIST = T.let({
   incompatible_version_format: "incompatible version format",
+  bumped_by_upstream:          "bumped by upstream",
 }.freeze, T::Hash[Symbol, String])
